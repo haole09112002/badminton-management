01a1e9e33834da4012659770b48b46c06889b7d3
haole09112002 <haole53492@gmail.com>
2025-07-28 20:05:21 +0700

    bugfix :: fix data type in model

--- backend/src/models/Member.ts
+++ backend/src/models/Member.ts
@@ -6,7 +6,7 @@ export interface MemberDocument extends mongoose.Document {
   name: string;
   balance: number;
   email: string;
-  password: string;
+  password?: string;
   role: 'user' | 'lead' | 'admin';
   comparePassword: (candidatePassword: string) => Promise<boolean>;
 }
@@ -35,11 +35,10 @@ const memberSchema = new mongoose.Schema({
     }
   }
 });
-memberSchema.pre<MemberDocument>('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
+memberSchema.methods.comparePassword = async function (candidatePassword: string) {
+  if (!this.password) return false;
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 memberSchema.methods.comparePassword = async function (candidatePassword: string) {
   return bcrypt.compare(candidatePassword, this.password);
